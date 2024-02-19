d23fab75164f36aef384da2b2a771499ca20ce57
Artem Nikiforov <nikart28@gmail.com>
2024-02-19 22:17:27 +0400

    Aws sqs publisher example

---
+++ 3.Sqs/SqsPublisher/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.Json;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using SqsPublisher;
+
+Console.WriteLine("Hello, World!");
+
+var sqsClient = new AmazonSQSClient();
+
+var customer = new CustomerCreated
+{
+    Id = Guid.NewGuid(),
+    Email = "nikart28@gmail.com",
+    FullName = "Artem Nikiforov",
+    DateOfBirth = new DateTime(1995, 8, 3),
+    GitHubUsername = "NikiforovArtem"
+};
+
+var queueUrlResponse = await sqsClient.GetQueueUrlAsync("customers");
+
+var sendMessageRequset = new SendMessageRequest
+{
+    QueueUrl = queueUrlResponse.QueueUrl,
+    MessageBody = JsonSerializer.Serialize(customer),
+    MessageAttributes = new Dictionary<string, MessageAttributeValue>
+    {
+        { "MessageType", new MessageAttributeValue { DataType = nameof(String), StringValue = nameof(CustomerCreated) } }
+    }
+};
+
+var response = await sqsClient.SendMessageAsync(sendMessageRequset);
+
+Console.WriteLine();
