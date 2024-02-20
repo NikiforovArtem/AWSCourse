597afa072652c212d10892bb6d12bd2c8f2a2466
Artem Nikiforov <nikart28@gmail.com>
2024-02-20 18:12:59 +0400

    adding a new part of course

---
+++ 4.Sns/SnsPublisher/CustomerCreated.cs
@@ -0,0 +1,14 @@
+﻿namespace SnsPublisher;
+
+public class CustomerCreated
+{
+    public required Guid Id { get; init; }
+    
+    public required string FullName { get; init; }
+    
+    public required string Email { get; set; }
+    
+    public required string GitHubUsername { get; set; }
+    
+    public required DateTime DateOfBirth { get; set; }
+}
---
+++ 4.Sns/SnsPublisher/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.Json;
+using Amazon.SimpleNotificationService;
+using Amazon.SimpleNotificationService.Model;
+using SnsPublisher;
+
+var snsClient = new AmazonSimpleNotificationServiceClient();
+
+var customer = new CustomerCreated
+{
+    Id = Guid.NewGuid(),
+    Email = "nikart28@gmail.com",
+    FullName = "Artem Nikiforov",
+    DateOfBirth = new DateTime(1995, 8, 3),
+    GitHubUsername = "NikiforovArtem"
+};
+
+var customerTopic = await snsClient.FindTopicAsync("customers");
+
+var publishRequest = new PublishRequest
+{
+    TopicArn = customerTopic.TopicArn,
+    Message = JsonSerializer.Serialize(customer),
+    MessageAttributes = new Dictionary<string, MessageAttributeValue>
+    {
+        { "MessageType", new MessageAttributeValue { DataType = nameof(String), StringValue = nameof(CustomerCreated) } }
+    }
+};
+
+var response = await snsClient.PublishAsync(publishRequest);
