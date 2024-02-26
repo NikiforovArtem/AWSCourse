916a54a1b829a9fa67c683a5c90c708a9d316d2a
Artem Nikiforov <nikart28@gmail.com>
2024-02-26 23:19:51 +0400

    add a pooling for an automatic update an api key

--- 7.SecretsManager/Weather.Api/Program.cs
+++ 7.SecretsManager/Weather.Api/Program.cs
@@ -12,6 +12,7 @@ builder.Configuration.AddSecretsManager(configurator: o =>
 {
     o.SecretFilter = entry => entry.Name.StartsWith($"{env}_{appName}");
     o.KeyGenerator = (_, s) => s.Replace($"{env}_{appName}_", string.Empty).Replace("__", ":");
+    o.PollingInterval = TimeSpan.FromDays(30);
 });
 
 builder.Services.AddControllers();
