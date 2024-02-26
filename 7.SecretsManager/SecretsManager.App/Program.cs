b8d41f68260bf00e40178543af01d92cfec6b6e8
Artem Nikiforov <nikart28@gmail.com>
2024-02-26 22:31:13 +0400

    add test porject with secret manager getting

---
+++ 7.SecretsManager/SecretsManager.App/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
+
+Console.WriteLine("Hello, World!");
+
+var secretsManager = new AmazonSecretsManagerClient();
+
+var getSecretRequest = new GetSecretValueRequest
+{
+    SecretId = "ApiKey"
+};
+
+var response = await secretsManager.GetSecretValueAsync(getSecretRequest);
+
+Console.WriteLine(response.SecretString);
