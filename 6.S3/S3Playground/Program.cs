c64e7109ad795152977fa1b31cbee3c71096f3dd
Artem Nikiforov <nikart28@gmail.com>
2024-02-26 22:02:26 +0400

    add a reading from s3

--- 6.S3/S3Playground/Program.cs
+++ 6.S3/S3Playground/Program.cs
@@ -1,17 +1,48 @@
-﻿using Amazon.S3;
+﻿using System.Text;
+using Amazon.S3;
 using Amazon.S3.Model;
 
 Console.WriteLine();
-var s3Client = new AmazonS3Client();
 
-using var inputStream = new FileStream("./movies.csv", FileMode.Open, FileAccess.Read);
+//await PutObjectToS3();
+await GetObjectFromS3();
+return;
 
-var putObjectRequest = new PutObjectRequest
+async Task PutObjectToS3()
 {
-    BucketName = "awscourseartemnik",
-    Key = "csv/movies.csv",
-    ContentType = "text/csv",
-    InputStream = inputStream
-};
+    var s3Client = new AmazonS3Client();
 
-await s3Client.PutObjectAsync(putObjectRequest);
+    using var inputStream = new FileStream("./movies.csv", FileMode.Open, FileAccess.Read);
+
+    var putObjectRequest = new PutObjectRequest
+    {
+        BucketName = "awscourseartemnik",
+        Key = "csv/movies.csv",
+        ContentType = "text/csv",
+        InputStream = inputStream
+    };
+
+    await s3Client.PutObjectAsync(putObjectRequest);
+}
+
+async Task GetObjectFromS3()
+{
+    var s3Client = new AmazonS3Client();
+    
+    var getObjectRequest = new GetObjectRequest
+    {
+        BucketName = "awscourseartemnik",
+        Key = "csv/movies.csv",
+    };
+
+    var response = await s3Client.GetObjectAsync(getObjectRequest);
+
+    using var memoryStream = new MemoryStream();
+
+    response.ResponseStream.CopyTo(memoryStream);
+
+    var text = Encoding.Default.GetString(memoryStream.ToArray());
+    
+    Console.WriteLine(text);
+}
+
