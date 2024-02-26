d260ad553ab067c0a7a856ff2cc9f182bcb63580
Artem Nikiforov <nikart28@gmail.com>
2024-02-26 23:15:03 +0400

    Good approach in storaging secrets and implement it in a web api app

--- 7.SecretsManager/Weather.Api/Program.cs
+++ 7.SecretsManager/Weather.Api/Program.cs
@@ -6,12 +6,21 @@ var builder = WebApplication.CreateBuilder(args);
 var env = builder.Environment.EnvironmentName;
 var appName = builder.Environment.ApplicationName;
 
+//Development_Weather.Api_OpenWeatherMapApi__ApiKey
+
+builder.Configuration.AddSecretsManager(configurator: o =>
+{
+    o.SecretFilter = entry => entry.Name.StartsWith($"{env}_{appName}");
+    o.KeyGenerator = (_, s) => s.Replace($"{env}_{appName}_", string.Empty).Replace("__", ":");
+});
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.Configure<OpenWeatherApiSettings>(builder.Configuration.GetSection(OpenWeatherApiSettings.Key));
 builder.Services.AddHttpClient();
 builder.Services.AddSingleton<IWeatherService, WeatherService>();
+
 
 var app = builder.Build();
 
