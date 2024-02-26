5f8a8155b3bc1c8b51551c92dd44a236a6059e63
Artem Nikiforov <nikart28@gmail.com>
2024-02-26 21:55:05 +0400

    test upload csv to s3

--- 6.S3/S3Playground/Program.cs
+++ 6.S3/S3Playground/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine();
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+
+Console.WriteLine();
+var s3Client = new AmazonS3Client();
+
+using var inputStream = new FileStream("./movies.csv", FileMode.Open, FileAccess.Read);
+
+var putObjectRequest = new PutObjectRequest
+{
+    BucketName = "awscourseartemnik",
+    Key = "csv/movies.csv",
+    ContentType = "text/csv",
+    InputStream = inputStream
+};
+
+await s3Client.PutObjectAsync(putObjectRequest);
