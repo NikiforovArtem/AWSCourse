c2b5421645244a33f0287364b231acdc880a6bbd
Artem Nikiforov <nikart28@gmail.com>
2024-02-27 19:23:32 +0400

    add a simple lambda

---
+++ 8.Lambda/SimpleLambda/src/SimpleLambda/Function.cs
@@ -0,0 +1,15 @@
+using Amazon.Lambda.Core;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace SimpleLambda;
+
+public class Function
+{
+    /// <returns></returns>
+    public void FunctionHandler(ILambdaContext context)
+    { 
+        context.Logger.LogInformation("Hello World From Lambda");
+    }
+}
